377a08ac22ca8627ea1e01b9d8d98c053f13e98f
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-03-14 11:10:04 +0300

    Add movie reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
-
+import movieReducer from 'features/Movies/Movies';
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    movieReducer,
   },
 });
